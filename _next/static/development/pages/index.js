9462cc735af4216f56ac2c4ae51f355dede95e76
Jakub Kubacki <j.kubacki@outlook.com>
2020-02-23 18:35:12 +0100

    Release compiled code

---
+++ _next/static/development/pages/index.js
@@ -0,0 +1,242 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/index.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fjakub%2FProjekty%2Fkbcki%2Fsrc%2Fpages%2Findex.tsx!./":
+/*!**************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fjakub%2FProjekty%2Fkbcki%2Fsrc%2Fpages%2Findex.tsx ***!
+  \**************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/", function() {
+      var mod = __webpack_require__(/*! ./src/pages/index.tsx */ "./src/pages/index.tsx")
+      if(true) {
+        module.hot.accept(/*! ./src/pages/index.tsx */ "./src/pages/index.tsx", function() {
+          if(!next.router.components["/"]) return
+          var updatedPage = __webpack_require__(/*! ./src/pages/index.tsx */ "./src/pages/index.tsx")
+          next.router.update("/", updatedPage)
+        })
+      }
+      return mod
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_ef0ff7c60362f24a921f ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_ef0ff7c60362f24a921f */ "dll-reference dll_ef0ff7c60362f24a921f"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./src/components/layout/Content/Content.tsx":
+/*!***************************************************!*\
+  !*** ./src/components/layout/Content/Content.tsx ***!
+  \***************************************************/
+/*! exports provided: Content */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Content", function() { return Content; });
+/* harmony import */ var _Content_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Content.scss */ "./src/components/layout/Content/Content.scss");
+/* harmony import */ var _Content_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_Content_scss__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/Users/jakub/Projekty/kbcki/src/components/layout/Content/Content.tsx";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_1__["createElement"];
+
+
+var Content = function Content(_ref) {
+  var children = _ref.children;
+  return __jsx("main", {
+    className: "kbcki-content",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, children);
+};
+
+/***/ }),
+
+/***/ "./src/components/layout/Sidebar/Sidebar.tsx":
+/*!***************************************************!*\
+  !*** ./src/components/layout/Sidebar/Sidebar.tsx ***!
+  \***************************************************/
+/*! exports provided: Sidebar */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Sidebar", function() { return Sidebar; });
+/* harmony import */ var _Sidebar_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Sidebar.scss */ "./src/components/layout/Sidebar/Sidebar.scss");
+/* harmony import */ var _Sidebar_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_Sidebar_scss__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/Users/jakub/Projekty/kbcki/src/components/layout/Sidebar/Sidebar.tsx";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_1__["createElement"];
+
+
+var Sidebar = function Sidebar() {
+  return __jsx("aside", {
+    className: "kbcki-sidebar",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, __jsx("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, "kbcki"));
+};
+
+/***/ }),
+
+/***/ "./src/components/layout/TwoSidedBackgroundContainer/TwoSidedBackgroundContainer.tsx":
+/*!*******************************************************************************************!*\
+  !*** ./src/components/layout/TwoSidedBackgroundContainer/TwoSidedBackgroundContainer.tsx ***!
+  \*******************************************************************************************/
+/*! exports provided: TwoSidedBackgroundContainer */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "TwoSidedBackgroundContainer", function() { return TwoSidedBackgroundContainer; });
+/* harmony import */ var _TwoSidedBackgroundContainer_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./TwoSidedBackgroundContainer.scss */ "./src/components/layout/TwoSidedBackgroundContainer/TwoSidedBackgroundContainer.scss");
+/* harmony import */ var _TwoSidedBackgroundContainer_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_TwoSidedBackgroundContainer_scss__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/Users/jakub/Projekty/kbcki/src/components/layout/TwoSidedBackgroundContainer/TwoSidedBackgroundContainer.tsx";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_1__["createElement"];
+
+
+var TwoSidedBackgroundContainer = function TwoSidedBackgroundContainer(_ref) {
+  var children = _ref.children;
+  return __jsx("div", {
+    className: "kbcki-two-sided-bg-container",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, __jsx("div", {
+    className: "kbcki-two-sided-bg-container__content",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, children), __jsx("div", {
+    className: "kbcki-two-sided-bg-container__bg-pane \\ kbcki-two-sided-bg-container__bg-pane--left",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }), __jsx("div", {
+    className: "kbcki-two-sided-bg-container__bg-pane \\ kbcki-two-sided-bg-container__bg-pane--right",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }));
+};
+
+/***/ }),
+
+/***/ "./src/pages/index.tsx":
+/*!*****************************!*\
+  !*** ./src/pages/index.tsx ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.scss */ "./src/pages/index.scss");
+/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_index_scss__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_layout_TwoSidedBackgroundContainer_TwoSidedBackgroundContainer__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/layout/TwoSidedBackgroundContainer/TwoSidedBackgroundContainer */ "./src/components/layout/TwoSidedBackgroundContainer/TwoSidedBackgroundContainer.tsx");
+/* harmony import */ var _components_layout_Content_Content__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../components/layout/Content/Content */ "./src/components/layout/Content/Content.tsx");
+/* harmony import */ var _components_layout_Sidebar_Sidebar__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../components/layout/Sidebar/Sidebar */ "./src/components/layout/Sidebar/Sidebar.tsx");
+var _jsxFileName = "/Users/jakub/Projekty/kbcki/src/pages/index.tsx";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0__["createElement"];
+
+
+
+
+
+
+var Index = function Index() {
+  return __jsx(_components_layout_TwoSidedBackgroundContainer_TwoSidedBackgroundContainer__WEBPACK_IMPORTED_MODULE_2__["TwoSidedBackgroundContainer"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, __jsx(_components_layout_Content_Content__WEBPACK_IMPORTED_MODULE_3__["Content"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, __jsx(_components_layout_Sidebar_Sidebar__WEBPACK_IMPORTED_MODULE_4__["Sidebar"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }), __jsx("div", {
+    style: {
+      padding: '64px'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }, "Work in progress")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Index);
+
+/***/ }),
+
+/***/ 1:
+/*!******************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fjakub%2FProjekty%2Fkbcki%2Fsrc%2Fpages%2Findex.tsx ***!
+  \******************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fjakub%2FProjekty%2Fkbcki%2Fsrc%2Fpages%2Findex.tsx! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fjakub%2FProjekty%2Fkbcki%2Fsrc%2Fpages%2Findex.tsx!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_ef0ff7c60362f24a921f":
+/*!*******************************************!*\
+  !*** external "dll_ef0ff7c60362f24a921f" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_ef0ff7c60362f24a921f;
+
+/***/ })
+
+},[[1,"static/runtime/webpack.js","styles"]]]);
+//# sourceMappingURL=index.js.map
