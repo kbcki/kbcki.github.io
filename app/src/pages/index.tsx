6758855f11bf9aa66ce8683a927571cd9066a60c
Jakub Kubacki <j.kubacki@outlook.com>
2020-02-23 22:02:30 +0100

    [v1.0.3] Add content

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -3,9 +3,9 @@ import Head from 'next/head';
 
 import './index.scss';
 import { TwoSidedBackgroundContainer } from '../components/layout/TwoSidedBackgroundContainer/TwoSidedBackgroundContainer';
-import { Content } from '../components/layout/Content/Content';
+import { Container } from '../components/layout/Container/Container';
 import { Sidebar } from '../components/layout/Sidebar/Sidebar';
-
+import { Content } from '../components/layout/Content/Content';
 
 const Index = () => (
     <>
@@ -15,16 +15,42 @@ const Index = () => (
                 name="viewport"
                 content="initial-scale=1.0, width=device-width" 
             />
+            <link rel="icon" type="image/png" href="favicon.png" />
         </Head>
         <TwoSidedBackgroundContainer>
-            <Content>
+            <Container>
                 <Sidebar />
-                <div style={{
-                    padding: '64px'
-                }}>
-                    Work in progress
-                </div>
-            </Content>
+                <Content>
+                    <h3>Hello</h3>
+                    <p>
+                        My name is Jakub Kubacki. I'm a web developer.&nbsp;
+                        <del>I work and live in Wrocław, Poland.</del>
+                        Since March 2020, I work remotely. My technology
+                        stack includes both frontend (React) and backend
+                        (Node.JS, .NET) technologies.
+                    </p>
+
+                    <h3>Job-status</h3>
+                    <p>
+                        hired
+                    </p>
+
+                    <h3>Why did I decide to create this page?</h3>
+                    <p>
+                        Tbh, I have no idea. I thought I've enough free time
+                        so I can write some blog posts. Well, it hasn't
+                        happened so far.
+                    </p>
+
+                    <h3>For some reason, I'd like to contact you</h3>
+                    <p>
+                        I think that LinkedIn would be the most convenient
+                        option. You can find my profile&nbsp;
+                        <a href="https://www.linkedin.com/in/jakub-kubacki/">
+                        here. </a>
+                    </p>
+                </Content>
+            </Container>
         </TwoSidedBackgroundContainer>
     </>
 );
