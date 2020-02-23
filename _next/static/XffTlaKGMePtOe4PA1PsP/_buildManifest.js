4f89568df8c26322beebe7504182b70823e55fa1
Jakub Kubacki <j.kubacki@outlook.com>
2020-02-23 21:51:10 +0100

    Removed prev release

--- _next/static/XffTlaKGMePtOe4PA1PsP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b]}}("static\u002Fcss\u002Fstyles.19e06948.chunk.css","static\u002Fchunks\u002Fstyles.39c60660ab53ccb48c0c.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
